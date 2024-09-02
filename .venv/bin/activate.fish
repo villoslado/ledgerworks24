e3cae60cb53ee3a1a665cc24f1def80246d55c72
Gerry <jg.villoslado@gmail.com>
2024-09-01 23:37:24 -0700

    updated venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gvilloslado/Projects/ledgerworks24/.venv"
+set -gx VIRTUAL_ENV "/Users/gvilloslado/Projects/GitHub/ledgerworks24/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
